bd825d473c612589e5ef8553210e9d1cc6ee9aeb
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2017-12-23 01:51:21 +0300

    issues fixed

--- run.py
+++ run.py
@@ -14,26 +14,28 @@ while (request != 0):
 		phones = cursor.fetchall()
 		print('ID', '\t', 'Model')
 		for i in range(len(phones)):
-			print(phones[i][0], '\t', phones[i][1])
+			print('{0}\t{1:40} {2}'.format(phones[i][0], phones[i][1], phones[i][2]))
 		phone_id = 0
 		try:
 			# Выбираем модель для подбора чехла.
-			print("Enter phone's id:"),
-			phone_id = input()
-			if int(phone_id) in range(len(phones)):
-				cur_size = phones[int(phone_id)][2]
+			print("Enter phone's id:", end=" ")
+			phone_id = int(input()) - 1
+			print("\n")
+			if phone_id in range(len(phones)):
+				cur_size = phones[phone_id][2]
 				print("Size: ", cur_size)
 				cursor.execute("SELECT model, price FROM cases WHERE ((low_size <="+str(cur_size)+") AND (high_size>="+str(cur_size)+")) ORDER BY price")
 				cases = cursor.fetchall()
-				print("Suitable model(s):")
+				print("Suitable model(s):\n")
 				for i in range(len(cases)):
 					print(cases[i][0],"  -  ",cases[i][1],"p.")
+				print("\n")
 			else:
-				print("Choose existing model")
+				print("Choose existing model\n")
 		except BaseException:
 			print("Error")
 	if int(request) == 2:
-		cursor.execute("select tmp.MODELS, tmp.Lcase from (select phones.model AS MODELS, cases.low_size AS Lcase from phones left join cases on ((phones.screen_size >= cases.low_size) and (phones.screen_size<=cases.high_size)) group by phones.model) AS tmp where tmp.Lcase is null")
+		cursor.execute("select tmp.MODELS from (select phones.model AS MODELS, cases.low_size AS Lcase from phones left join cases on ((phones.screen_size >= cases.low_size) and (phones.screen_size<=cases.high_size)) group by phones.model) AS tmp where tmp.Lcase is null")
 		no_cases = cursor.fetchall()
 		print("Devices with no suitable cases: ")
 		for i in range(len(no_cases)):
@@ -42,8 +44,9 @@ while (request != 0):
 			print("Potential contributors are:")
 			cursor.execute("SELECT * FROM (SELECT nam, DATE_ADD(contributers.last_delievery, INTERVAL contributers.deliev_freq DAY) as C_DATE, phone FROM contributers order by C_DATE) AS tmp WHERE C_DATE > CURDATE() limit 3")
 			potential_contributors = cursor.fetchall()
+			print("\nContributor\t\tDate\t\tPhone")
 			for i in range(len(potential_contributors)):
-				print(potential_contributors[i][0], potential_contributors[i][1])
+				print('{0:20} {1:18} {2}'.format(potential_contributors[i][0], str(potential_contributors[i][1]), potential_contributors[i][2]))
 
 cnx.close()
 a = input()
